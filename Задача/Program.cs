5f3f2e36c9598103250d6c9747c140cf15cad669
Julia <KorolevaJulia@yandex.ru>
2022-12-01 20:11:49 +0300

    Заданы массивы и максимальный размер строки

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -9,4 +9,11 @@
 ["Russia", "Denmark", "Kazan"] -> []
 */
 
+string [] array1 = {"hello", "2", "world", ":-)"};
+string [] array2 = {"1234", "1567", "-2", "computer science"};
+string [] array3 = {"Russia", "Denmark", "Kazan"};
+
+int maxLength = 3;
+
+
 
