8ed2b20d095469b6089d97990644ef08a39346bc
Julia <KorolevaJulia@yandex.ru>
2022-12-04 17:50:18 +0300

    Метод отбора из исходного массива

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -9,11 +9,30 @@
 ["Russia", "Denmark", "Kazan"] -> []
 */
 
-string [] array1 = {"hello", "2", "world", ":-)"};
-string [] array2 = {"1234", "1567", "-2", "computer science"};
-string [] array3 = {"Russia", "Denmark", "Kazan"};
-
+string [] array = {"hello", "2", "world", ":-)"};
 int maxLength = 3;
+
+PrintArray(array);
+
+SelectArray(array);
+
+//Метод отбора из массива строк с количеством символов менее или равно 3
+void SelectArray(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+
+    for(int i = 0; i < length; i++)
+    {
+        if(array[i].Length <= maxLength)
+        {
+            result[count] = array[i];
+            Console.Write($" {result[count]}");
+            count++;
+        }  
+    }
+}
 
 
 
